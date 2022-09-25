ef978ea5c4f7d87782c4f7da8a34178ef34972c1
Franco Ruggeri <francoruggeri95@gmail.com>
2022-09-25 16:11:13 +0200

    Complete easy, medium, and hard extra challenges of prototype 1

--- Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 5, -7);
+    
+    private List<Vector3> offsets = new List<Vector3>
+    {
+        new Vector3(0, 5, -7),
+        new Vector3(0, 2.5f, 0.5f)
+    };
+    private int offsetId = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -13,9 +19,17 @@ public class FollowPlayer : MonoBehaviour
         
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        bool keyPressed = Input.GetButtonDown("Fire1");
+        if (keyPressed)
+            offsetId = (offsetId + 1) % offsets.Count;
+    }
+
     // LastUpdate is called once per frame, after Update
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        transform.position = player.transform.position + offsets[offsetId];
     }
 }
